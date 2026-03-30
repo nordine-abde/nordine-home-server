95f023a77966f2bfeceef1af11630dc41bfd674e
a.nordine <abdessamad.nordine.github@gmail.com>
2026-03-30 23:00:53 +0200

    fix docker-entrypoint.sh

--- config/caddy/docker-entrypoint.sh
+++ config/caddy/docker-entrypoint.sh
@@ -5,15 +5,13 @@ set -eu
 : "${DOMAIN:?DOMAIN must be set}"
 
 AUTO_SSL="${AUTO_SSL:-true}"
-GLOBAL_OPTIONS=""
 REDIRECT_SCHEME="https"
 
 if [ "$AUTO_SSL" = "false" ]; then
-  GLOBAL_OPTIONS="$(printf '{\n  auto_https off\n}\n')"
   REDIRECT_SCHEME="http"
 fi
 
-export DOMAIN GLOBAL_OPTIONS REDIRECT_SCHEME
+export DOMAIN REDIRECT_SCHEME
 envsubst < /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
 
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
