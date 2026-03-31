1edceffd409c25ad7de7bc026cff8f4bf6efbcfe
a.nordine <abdessamad.nordine.github@gmail.com>
2026-03-31 16:28:58 +0200

    improve dns service forwarding setup

--- config/dns/docker-entrypoint.sh
+++ config/dns/docker-entrypoint.sh
@@ -4,7 +4,9 @@ set -eu
 
 : "${IP:?IP must be set}"
 : "${DOMAIN:?DOMAIN must be set}"
+: "${UPSTREAM_DNS_1:?UPSTREAM_DNS_1 must be set}"
+: "${UPSTREAM_DNS_2:?UPSTREAM_DNS_2 must be set}"
 
-envsubst '${IP} ${DOMAIN}' < /tmp/dnsmasq.conf.template > /etc/dnsmasq.conf
+envsubst '${IP} ${DOMAIN} ${UPSTREAM_DNS_1} ${UPSTREAM_DNS_2}' < /tmp/dnsmasq.conf.template > /etc/dnsmasq.conf
 
 exec dnsmasq --keep-in-foreground --conf-file=/etc/dnsmasq.conf
