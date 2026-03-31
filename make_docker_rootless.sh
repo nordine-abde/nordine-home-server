0aecf14d181180abfbd5454863e76877e81a3c7f
a.nordine <abdessamad.nordine.github@gmail.com>
2026-03-31 17:16:54 +0200

    fix docker-compose.yaml typo and install new newuidmap before rootless docker setup

--- make_docker_rootless.sh
+++ make_docker_rootless.sh
@@ -1,3 +1,8 @@
+sudo sh -eux <<EOF
+# Install newuidmap & newgidmap binaries
+apt-get install -y uidmap
+EOF
+
 dockerd-rootless-setuptool.sh install
 
 sudo systemctl disable --now docker.service docker.socket docker
